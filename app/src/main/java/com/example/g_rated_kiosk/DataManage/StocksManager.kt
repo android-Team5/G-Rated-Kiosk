c9462a95a54d4d34c9fd940f809ac9a59387f1e6
KSC <ksc1008@naver.com>
2022-12-10 17:27:37 +0900

    delete uselesses

--- app/src/main/java/com/example/g_rated_kiosk/DataManage/StocksManager.kt
+++ app/src/main/java/com/example/g_rated_kiosk/DataManage/StocksManager.kt
@@ -2,15 +2,46 @@ package com.example.g_rated_kiosk.DataManage
 
 import com.example.g_rated_kiosk.Menu
 import com.example.g_rated_kiosk.cart
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.type.TimeOfDay
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.Date
 
 class StocksManager {
+    data class salesData(val price:Int, val quantitySold:Int, val time:String)
     companion object {
+
+        lateinit var today:String
+
         fun sellStock(soldItems:List<cart>){
+            for(i in soldItems){
+                i.menu?.let{
+                    sellStock(it,i.count)
+                }
+
+                i.side?.let {
+                    sellStock(it,i.count)
+                }
+
+                i.drink?.let {
+                    sellStock(it,i.count)
+                }
+            }
 
         }
 
+        fun makeToday(){
+            val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+            today = LocalDateTime.now().format(dateFormatter)
+        }
+
         fun sellStock(soldMenu: Menu, quantity:Int){
+
+
+            val item = Firebase.firestore.collection("stock").document("sales")
+                .collection(today).add(salesData(soldMenu.Price,quantity,"test"))
 
         }
 
