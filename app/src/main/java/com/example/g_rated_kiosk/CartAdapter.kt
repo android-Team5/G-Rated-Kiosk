f61d4270490a376fff62653aa23413d3fe5ba8e2
Noah <seohyun060@naver.com>
2022-12-14 19:38:52 +0900

    itemXML
    publishing done

--- app/src/main/java/com/example/g_rated_kiosk/CartAdapter.kt
+++ app/src/main/java/com/example/g_rated_kiosk/CartAdapter.kt
@@ -30,6 +30,15 @@ class CartAdapter (val cartList: MutableList<cart>): RecyclerView.Adapter<Recycl
         if(cartList[position].drink!=null){
             itemBinding.itemDrinkName.text= cartList[position].drink?.Name
         }
+        if(cartList[position].noLettuce){
+            itemBinding.itemNoLettuce.text = "양배추 X"
+        }
+        if(cartList[position].noOnion){
+            itemBinding.itemNoOnion.text = "양파 X"
+        }
+        if(cartList[position].noPickle){
+            itemBinding.itemNoPickle.text = "피클 X"
+        }
         itemBinding.itemRoot.setOnClickListener { // 뷰에 이벤트 추가
         }
       itemBinding.itemAdd.setOnClickListener { cartList[position].count +=1
