12d7b3c875236d26873d61355a1f088963151b2c
Noah <seohyun060@naver.com>
2022-12-15 01:22:38 +0900

    design publishing done

--- app/src/main/java/com/example/g_rated_kiosk/FromHereToGo.kt
+++ app/src/main/java/com/example/g_rated_kiosk/FromHereToGo.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.g_rated_kiosk.databinding.FhtgBinding
 
-class MainActivity2 : AppCompatActivity() {
+class FromHereToGo : AppCompatActivity() {
     var eatWhere=""
     var logoClick=0
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,18 +21,19 @@ class MainActivity2 : AppCompatActivity() {
         fhtgBinding.fromHere.setOnClickListener {
 
             eatWhere = "fromHere"
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, MenuSelect::class.java)
             startActivity(intent);
         }
         fhtgBinding.toGo.setOnClickListener {
             eatWhere = "toGo"
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, MenuSelect::class.java)
             startActivity(intent);
         }
         fhtgBinding.toGo.setOnLongClickListener {
 
 
-            if (logoClick==5){
+            if (logoClick>=5){
+                logoClick=0;
                 val intent = Intent(this, AdminActivity::class.java)
                 startActivity(intent);
             }
@@ -40,12 +41,8 @@ class MainActivity2 : AppCompatActivity() {
             return@setOnLongClickListener true;
         }
         fhtgBinding.logo.setOnClickListener {
-            if(logoClick==5){
-                logoClick=0
-            }
-            else{
+
                 logoClick +=1
-            }
         }
 
 
