46256085f1ac221c8b8b1d51942b0d06ab6acc30
Noah <seohyun060@naver.com>
2022-12-14 17:18:18 +0900

    single 수정

--- app/src/main/java/com/example/g_rated_kiosk/SingleActivity.kt
+++ app/src/main/java/com/example/g_rated_kiosk/SingleActivity.kt
@@ -12,60 +12,7 @@ class SingleActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(/* layoutResID = */ R.layout.single)
 
-        var lettuceCnt = 1
-        var onionCnt = 1
-        var pickleCnt = 1
 
-        add1.setOnClickListener {
-//            if(lettuceCnt == 1) {
-//                extraPrice1.text = "+ 400원"
-//                lettuceCnt ++
-//            }
-//            else if(lettuceCnt == 2)
-//            {
-//                extraPrice1.text = "+ 800원"
-//                lettuceCnt ++
-//            }
-//            else if(lettuceCnt == 3)
-//            {
-//                extraPrice1.text = "+ 1200원"
-//                lettuceCnt ++
-//            }
-        }
-
-        add2.setOnClickListener {
-//            if(onionCnt == 1) {
-//                extraPrice2.text = "+ 400원"
-//                onionCnt ++
-//            }
-//            else if(onionCnt == 2)
-//            {
-//                extraPrice2.text = "+ 800원"
-//                onionCnt ++
-//            }
-//            else if(onionCnt == 3)
-//            {
-//                extraPrice2.text = "+ 1200원"
-//                onionCnt ++
-//            }
-        }
-
-        add3.setOnClickListener {
-//            if(pickleCnt == 1) {
-//                extraPrice3.text = "+ 400원"
-//                pickleCnt ++
-//            }
-//            else if(pickleCnt == 2)
-//            {
-//                extraPrice3.text = "+ 800원"
-//                pickleCnt ++
-//            }
-//            else if(pickleCnt == 3)
-//            {
-//                extraPrice3.text = "+ 1200원"
-//                pickleCnt ++
-//            }
-        }
 
         del1.setOnClickListener {
             if (!Common.chosenMenu.noLettuce)
@@ -78,18 +25,7 @@ class SingleActivity: AppCompatActivity() {
                 del1.setBackgroundColor(Color.parseColor("#b5b3b3"));
             }
 
-//            lettuceCnt -= 1
-//            if(lettuceCnt == 1) {
-//                extraPrice1.text = "+ 400원"
-//            }
-//            else if(lettuceCnt == 2)
-//            {
-//                extraPrice1.text = "+ 800원"
-//            }
-//            else if(lettuceCnt == 0)
-//            {
-//                extraPrice1.text = "+ 0원"
-//            }
+//
         }
 
 
@@ -104,18 +40,7 @@ class SingleActivity: AppCompatActivity() {
                 del2.setBackgroundColor(Color.parseColor("#b5b3b3"));
             }//            onionCnt -= 1
 //
-//            if(onionCnt == 1) {
-//                extraPrice2.text = "+ 400원"
-//
-//            }
-//            else if(onionCnt == 2)
-//            {
-//                extraPrice2.text = "+ 800원"
-//            }
-//            else if(onionCnt == 0)
-//            {
-//                extraPrice2.text = "+ 0원"
-//            }
+
 
         }
 
@@ -130,18 +55,7 @@ class SingleActivity: AppCompatActivity() {
                 Common.chosenMenu.noPickle = false;
                 del3.setBackgroundColor(Color.parseColor("#b5b3b3"));
             }//            pickleCnt -= 1
-//            if(pickleCnt == 1) {
-//                extraPrice3.text = "+ 400원"
-//            }
-//            else if(pickleCnt == 2)
-//            {
-//                extraPrice3.text = "+ 800원"
-//            }
-//            else if(pickleCnt == 0)
-//            {
-//                extraPrice3.text = "+ 0원"
-//            }
-
+//
         }
         okButton2.setOnClickListener{
             Common.chosenMenu.count=1;
